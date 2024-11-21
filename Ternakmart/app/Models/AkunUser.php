5d79e2498743953aebf846eb38977789dac3569a
sandynicholas <sandynicholas1201@gmail.com>
2024-11-21 11:05:51 +0700

    Setup Account Models

---
+++ Ternakmart/app/Models/AkunUser.php
@@ -0,0 +1,18 @@
+// app/Models/AkunUser.php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class AkunUser extends Authenticatable
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nama', 'email', 'nomer_telepon', 'username', 'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+}

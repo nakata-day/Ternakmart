6f92d6dd1b5c40f74e8cb47e8f7c93776ed419ef
sandynicholas <sandynicholas1201@gmail.com>
2024-11-21 15:02:48 +0700

    Re-structure Folder

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://ternakmart.test/'),
+    'url' => env('APP_URL', 'http://ternakmart.test/Ternakmart/'),
 
     'asset_url' => env('ASSET_URL'),
 
